4f78045dafefd85e86740044a70d0749aa1a2125
Giuseppe <g.mattera@yahoo.com>
2024-02-24 17:58:15 +0100

    Express_server_update

--- Exercises/Node.js/ex_12_simpleExpressServer/simple_Express_server/src/server.js
+++ Exercises/Node.js/ex_12_simpleExpressServer/simple_Express_server/src/server.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import "express-async-errors";
 import morgan from "morgan";
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 app.use(morgan("dev"));
 
@@ -17,10 +17,17 @@ let planets = [
   },
 ];
 
-app.get('/', (req, res) => {
+app.get('/api/planets', (req, res) => {
   res.status(200).json(planets)
-})
+});
+
+app.get('/api/planets/:id', (req, res) => {
+  const { id } = req.params;
+  const planet = planets.find((element) => element.id === Number(id));
+
+  res.status(200).json(planet)
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
-})
+});
